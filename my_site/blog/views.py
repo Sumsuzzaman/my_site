2ba90fe8e229d0fe92ea8d7494a30f1d8168c1c6
Sumsuzzaman Chowdhury <joysho@gmail.com>
2024-03-05 00:10:33 +0600

    all css added

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -8,8 +8,8 @@ def index(request):
     
 
 def posts(request):
-    # return render(request, 'posts.html')
-    pass
+    return render(request, 'blog/all-posts.html')
+    
 
 def post_detail(request):
     # return render(request, 'post.html')
